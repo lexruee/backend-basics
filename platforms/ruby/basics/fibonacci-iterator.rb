2748d6f33fe5b7d67033dc501902fd50886a5ba4
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2018-05-31 21:14:42 +0200

    add another yield and iterator example
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

---
+++ platforms/ruby/basics/fibonacci-iterator.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+def fib(n)
+    ary = [0, 1]
+    a, b = ary
+    if block_given?
+        yield 0
+        yield 1
+    end
+    while n > 0 do
+        a, b = b, a + b
+        ary << b
+        yield b if block_given?
+        n -= 1
+    end
+    ary
+end
+
+
+fib(11) do |v|
+ puts "#{v}"
+end
+
+p fib(11)
