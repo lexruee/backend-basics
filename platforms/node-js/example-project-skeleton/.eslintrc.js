8a941a0c7f8162081e5c27f4bbacd4bc508be06a
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2018-06-15 22:08:55 +0200

    add example skeleton project
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

---
+++ platforms/node-js/example-project-skeleton/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "comma-dangle": [
+            2, 
+            "always-multiline"
+        ],
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            0,
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
