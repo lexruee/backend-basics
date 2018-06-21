21a0b9f068a8996f15f54d2c280a3d1ccf0c04bf
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2018-06-21 02:54:27 +0200

    add es6-basics directory
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

---
+++ platforms/node-js/es6-basics/README.md
@@ -0,0 +1,14 @@
+= ES6 Examples
+
+== Dependencies
+
+```
+npm install -g gulp eslint
+npm install
+```
+
+== Builiding
+
+```
+gulp
+```
---
+++ platforms/node-js/es6-basics/es6/test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const sequences = [
+    { subject: "JavaScript", verb: "is", object: "great"},
+    { subject: "Elephants", verb: "are", object: "large"},
+];
+
+function say({subject, verb, object}) {
+    console.log(`${subject} ${verb} ${object}`);
+}
+
+for(let s of sequences) {
+    say(s);
+}
