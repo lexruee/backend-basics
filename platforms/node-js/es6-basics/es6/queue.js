9fde243394aa7fc4d914dfaa7bc2f4c6575c3978
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2018-06-22 04:41:58 +0200

    add queue and stack example
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

---
+++ platforms/node-js/es6-basics/es6/queue.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let arr = [0, 1, 2, 3, 4];
+console.log(arr);
+
+let shifted = arr.shift();
+console.log(shifted);
+console.log(arr);
+
+arr.unshift(0);
+console.log(arr);
+
---
+++ platforms/node-js/es6-basics/es6/stack.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let arr = [0, 1, 2, 3, 4];
+console.log(arr);
+
+arr.push(5);
+console.log(arr);
+
+let poped = arr.pop();
+console.log(poped);
+console.log(arr);
