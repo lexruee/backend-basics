fe03e06e5eddc558f4fce91c00843c40461a3e68
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2018-06-25 03:05:43 +0200

    add generator example
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

---
+++ platforms/node-js/es6-basics/es6/generator.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function* rainbow() {
+    yield 'red';
+    yield 'orange';
+    yield 'yellow';
+    yield 'green';
+    yield 'blue';
+    yield 'indigo';
+    yield 'violet';
+}
+
+let it = rainbow();
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+for(let color of rainbow()) {
+    console.log(color);
+}
